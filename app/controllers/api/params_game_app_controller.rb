c737ead05740e938dad8efcd124f53b2a7fee9bc
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-25 15:21:44 -0500

    adds views

--- app/controllers/api/params_game_app_controller.rb
+++ app/controllers/api/params_game_app_controller.rb
@@ -1,8 +1,8 @@
 class Api::ParamsGameAppController < ApplicationController
-  def params_game_app
+  def params_game_action
     input_name = params["my_name"]
-    output_message = "my name is #{my_name}"
+    @output_message = "my name is #{input_name}"
 
-    render "params_game.json.jb"
+    render "params_game_app.json.jb"
   end
 end
