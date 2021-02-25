ccfbac74dd63f339f91b6eb129c2d42c4a73a3d4
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-25 15:29:42 -0500

    creates query option to recognize first letter of input name

--- app/controllers/api/params_game_app_controller.rb
+++ app/controllers/api/params_game_app_controller.rb
@@ -1,8 +1,11 @@
 class Api::ParamsGameAppController < ApplicationController
   def params_game_action
     input_name = params["my_name"]
-    @output_message = "my name is #{input_name}"
-
+    if input_name[0].downcase == "a"
+      @output_message = "Your first name is #{input_name}, and it begins with the letter #{input_name[0]}!"
+    else
+      @output_message = "my name is #{input_name}"
+    end
     render "params_game_app.json.jb"
   end
 end
